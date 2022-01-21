4bc66d9455585d65e5052d2c41272e150fd45b3b
kded <kded300@gmail.com>
2022-01-21 15:26:33 +0000

    Adds utility class for dealing with BigDecimal's

---
+++ src/main/java/wright/john/Utils.java
@@ -0,0 +1,33 @@
+package wright.john;
+
+import wright.john.model.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Utils {
+
+    /**
+     * Convenience method to sum BigDecimals.
+     * @param numbers
+     * @return
+     */
+    public static BigDecimal addBigDecimalsFromItems(List<Item> numbers) {
+        return numbers.stream().map(item -> item.getPrice())
+                .reduce(BigDecimal.ZERO,BigDecimal::add);
+    }
+
+    /**
+     * Remove extra trailing zeros from BigDecimals to avoid failing an equality check when the number is equivalent.
+     * @param value
+     * @return
+     */
+    public static BigDecimal stripDecimalZeros(BigDecimal value ) {
+        if ( value == null )
+            return null;
+
+
+        BigDecimal striped = ( value.scale() > 0 ) ? value.stripTrailingZeros() : value;
+        return ( striped.scale() < 0 ) ? striped.setScale( 0 ) : striped;
+    }
+}
---
+++ src/test/java/wright/john/UtilsTest.java
@@ -0,0 +1,30 @@
+package wright.john;
+
+import org.junit.jupiter.api.Test;
+import wright.john.model.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UtilsTest {
+
+    @Test
+    public void shouldSumBigDecimals() {
+        List<Item> items =List.of(Item.createItem('A'),Item.createItem('B'),Item.createItem('C'));
+        items.get(0).setPrice(BigDecimal.valueOf(12.5));
+
+        BigDecimal result = Utils.addBigDecimalsFromItems(items);
+
+        assertThat(result).isEqualTo(BigDecimal.valueOf(62.5));
+    }
+
+    @Test
+    public void shouldStripTrailingZeros() {
+        BigDecimal trailZeroDecimal = Utils.stripDecimalZeros(BigDecimal.valueOf(125.00));
+        BigDecimal comparison = BigDecimal.valueOf(125);
+        assertThat(trailZeroDecimal).toString().equals("125");
+        assertThat(trailZeroDecimal).isEqualTo(comparison);
+    }
+}
